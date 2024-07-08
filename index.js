3889b345ddbafe4980a81d92f2e2df17fece3f6b
Mario Castro <mcast032@gmail.com>
2024-07-08 14:39:51 -0700

    working fetch

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Connected")
-})
+document.addEventListener('DOMContentLoaded', () => fetchData());
+
+function fetchData() {
+    fetch('https://rickandmortyapi.com/api/character')
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data); // Log the data here
+        });
+}
+
+console.log(fetchData); // This will log the fetchData function definition, not the fetched data
